c8f78f13cd4a2d27792710da58eb9de169a8c2d8
rwurdig <rwurdig@gmail.com>
2025-10-27 04:52:29 -0300

    Load OPENAI_API_KEY from environment in appconfig

--- week_1/appconfig.py
+++ week_1/appconfig.py
@@ -7,7 +7,9 @@ load_dotenv(override=True)
 class EnvConfig():
 
     def __init__(self):
+        # Load API keys from environment
         self.groq_api_key = os.getenv("GROQ_API_KEY")
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # instantiate environment vars
 env_config = EnvConfig()
